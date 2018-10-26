6ffe4a46c21ceeefe7b57424bdab3b678ccbc66e
xvzezi <xvzezi@hotmail.com>
2018-10-26 13:03:33 +0800

    problem1 run

--- src/project/Application.java
+++ src/project/Application.java
@@ -1,8 +1,19 @@
 package project;
+import project.code.*;
 
 public class Application {
 	public static void main(String[] args)
 	{
+		// -------------------------- //
 		System.out.println("Hello World!");
+		
+		// -------------------------- //
+		MultiLineProblem solver = new MultiLineProblem();
+		Double k = solver.evalFarthestCrossPointOnThreeLines(1, 2, 3, 4, 5, 6, 7, 8, 9);
+		if(k == null) {
+			System.out.println("Something Wrong");
+		}else {
+			System.out.println(k);
+		}
 	}
 }
--- src/project/code/MultiLineProblem.java
+++ src/project/code/MultiLineProblem.java
@@ -81,5 +81,6 @@ public class MultiLineProblem {
 		return "Ò»°ãÈý½ÇÐÎ";
 	
 	}
+	
 
 }
