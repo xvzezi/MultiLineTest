f1513301c3b888b30a73a625002e56f8d75ecd32
xvzezi <xvzezi@hotmail.com>
2018-10-27 14:40:04 +0800

    version 0.6

---
+++ src/project/util/UseCaseGenerator.java
@@ -0,0 +1,11 @@
+package project.util;
+
+import java.util.ArrayList;
+
+public class UseCaseGenerator {
+	public static void main(String[] args) {
+		int[] a = {-1, 0, 100, 400, 600};
+		int[] b = {-1, 0, 50, 200, 600};
+		int[] c = {1, 4, 8,
+	}
+}
