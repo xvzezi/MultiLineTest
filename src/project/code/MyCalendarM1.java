d9200aea91732d0b8d1a34e86cf19de46cd6fa1b
liukecheng <liukecheng@192.168.33.117>
2018-11-17 18:25:29 +0800

    MyCalendarM0-5

---
+++ src/project/code/MyCalendarM1.java
@@ -0,0 +1,173 @@
+package project.code;
+//template
+/***FAULT## FAILURE INDUCING CODE***/
+/* INDUCING Detail: mutation 
+* ** original code **:
+* 
+* ---------------------
+* ** mutated code **:
+* 
+*/
+
+//Mutation 1
+
+public class MyCalendarM1 {
+	
+	public boolean isLeapYear(int year) {
+		/***FAULT## FAILURE INDUCING CODE***/
+		/* INDUCING Detail: mutation 
+		* ** original code **:
+		* if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+		* ---------------------
+		* ** mutated code **:
+		* if(year % 4 == 0 || year % 100 != 0 || year % 400 == 0)
+		*/
+		if(year % 4 == 0 || year % 100 != 0 || year % 400 == 0)
+			return true;
+		else 
+			return false;
+	}
+	
+	public int daysOfMonth(int year, int month) throws Exception {
+		month += 1;
+		switch(month) {
+		case 1:
+			return 31;
+		case 2:
+			if(isLeapYear(year))
+				return 29;
+			else
+				/***FAULT## FAILURE INDUCING CODE***/
+				/* INDUCING Detail: mutation 
+				* ** original code **:
+				* return 28;
+				* ---------------------
+				* ** mutated code **:
+				* return 29;
+				*/
+				return 29;
+		case 3:
+			return 31;
+		case 4:
+			return 30;
+		case 5:
+			return 31;
+		case 6:
+			return 30;
+		case 7:
+			return 31;
+		case 8:
+			return 31;
+		case 9:
+			return 30;
+		case 10:
+			return 31;
+		case 11:
+			return 30;
+		case 12:
+			return 31;
+		default:
+			throw new Exception("Invalid Month");
+		}
+	}
+	
+	public int weekendsOfMonth(int year, int month) throws Exception {
+		int days = daysOfMonth(year, month);
+		int weekends = 0;
+		/***FAULT## FAILURE INDUCING CODE***/
+		/* INDUCING Detail: mutation 
+		* ** original code **:
+		* for(int i = 1; i <= days; i ++) {
+		* ---------------------
+		* ** mutated code **:
+		* for(int i = 1; i < days; i ++) {
+		*/
+		for(int i = 1; i < days; i ++) {
+			int dayOfWeek = getWeek(year, month, i);
+			if(dayOfWeek == 0 || dayOfWeek == 6)
+				weekends ++;
+		}
+		return weekends;
+	}
+	
+	/**
+	 * 计算任意时间是星期几
+	 * 利用蔡勒公式
+	 * 公式：（年份代码+月份代码+日期）mod 7
+	 * @param year
+	 * @param month(0-11)
+	 * @param day
+	 * @return
+	 */
+	public int getWeek(int year,int month,int day){
+		month += 1;
+		while(year < 1700 || year > 2099){
+			if(year < 1700){
+				year += 400;
+			}else if(year > 2099){
+				year -= 400;
+			}
+		}
+		int x = year % 1000% 100;
+		/***FAULT## FAILURE INDUCING CODE***/
+		/* INDUCING Detail: mutation 
+		* ** original code **:
+		* int srcYear = (x / 4 + x) % 7;
+		* ---------------------
+		* ** mutated code **:
+		* int srcYear = (x * 4 + x) % 7;
+		*/
+		int srcYear = (x * 4 + x) % 7;
+		if(year >= 1700 && year <= 1799){
+			srcYear += 5;
+		}else if(year >= 1800 && year <= 1899){
+			srcYear += 3;
+		}else if(year >= 1900 && year <= 1999){
+			srcYear += 1;
+		}
+		int srcMonth = 0;
+		switch(month){
+		case 5:
+			srcMonth = 0;
+			break;
+		case 8:
+			srcMonth = 1;
+			break;
+		case 2:
+		case 3:
+		case 11:
+			srcMonth = 2;
+			break;
+		case 6:
+			srcMonth = 3;
+			break;
+		case 9:
+		case 12:
+			srcMonth = 4;
+			break;
+		case 4:
+		case 7:
+			srcMonth = 5;
+			break;
+		case 1:
+		case 10:
+			srcMonth = 6;
+			break;
+		}
+		//如果是闰年，改变月份代码的值
+		if(isLeapYear(year)){
+			switch(month){
+			case 1:
+				srcMonth = 5;
+				break;
+			case 2:
+				srcMonth = 1;
+				break;
+					
+			}
+		}
+		int week = (srcYear + srcMonth + day) % 7;
+		return week;
+	}
+
+}
